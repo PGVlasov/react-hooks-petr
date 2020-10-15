b8f15203666a6e5bd0a7a3e37dca9ccf96107234
PGVlasov <PGVlasov@users.noreply.github.com>
2020-10-15 12:04:12 +0300

    first commit

--- .history/src/App_20201015114656.js
+++ .history/src/App_20201015114656.js
@@ -3,11 +3,7 @@ import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
-  return (
-    <div>
-      <h1>Project1</h1>
-    </div>
-  );
+  return <div></div>;
 }
 
 export default App;
